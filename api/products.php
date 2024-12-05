f94301d8ce124a70b256dd8a3676b7ab67e18ff8
amrrdev <amrrdev@gmail.com>
2024-12-05 22:43:15 +0200

    Added authentication check using JWT service to restrict access to authenticated users only.

--- api/products.php
+++ api/products.php
@@ -1,11 +1,12 @@
 <?php
 require_once "./../config/database.php";
 require_once "./../services/product.service.php";
-
+require_once "./../services/jwt.service.php";
 $database = new Database();
 $connection = $database->getConnection();
 
 $productService = new ProductService($connection);
+$jwtService = new JWTService();
 
 if ($_SERVER['REQUEST_METHOD'] === "GET") {
     header('Content-Type: application/json');
@@ -13,9 +14,21 @@ if ($_SERVER['REQUEST_METHOD'] === "GET") {
         $id = (int) $_GET['id'];
         echo $productService->getProductById($id);
     } else {
+        /**
+         *  Check if the user is authenticated by calling the `isAuthenticated` method from the JWT service.
+         *  If the method does not return `true`, output the response (e.g., error message) and terminate the execution.
+         *  This ensures that only authenticated users can proceed with the requested operation.
+
+         */
+        // $authResponse = $jwtService->isAuthenticated();
+        // if ($authResponse !== true) {
+        //     echo $authResponse;
+        //     return;
+        // }
         echo $productService->getAllProducts();
     }
 }
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     header('Content-Type: application/json');
