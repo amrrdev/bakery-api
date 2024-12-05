1d43046d71e4cb2716dfdd2622b03ab9e5ea51d6
amrrdev <amrrdev@gmail.com>
2024-12-05 23:35:26 +0200

    Implemented PaymentService with method to create new payments, including error handling for missing information and database insertion failures

---
+++ services/payment.service.php
@@ -0,0 +1,36 @@
+<?php
+
+class PaymentService
+{
+    private mysqli $mysqli;
+
+    public function __construct($mysqli)
+    {
+        $this->mysqli = $mysqli;
+    }
+
+    public function CreateNewPyament($card_number, $cvv, $name_on_card)
+    {
+        try {
+            if (!isset($card_number, $cvv, $name_on_card)) {
+                throw new Exception("Missing Information, Please provide (card number, cvv, name on card)");
+            }
+            $userId = $_SESSION['user'];
+            $query = "INSERT INTO payments (card_number, cvv, name_on_card, user_id) VALUES (?, ?, ?, ?)";
+            $statement = $this->mysqli->prepare($query);
+            $statement->bind_param("sssi", $card_number, $cvv, $name_on_card, $userId);
+            $statement->execute();
+
+            if ($statement->affected_rows === 0) {
+                throw new Exception("Failed to create product");
+            }
+
+            $statement->close();
+            http_response_code(201);
+            return json_encode(["success" => "Product created successfully"]);
+        } catch (Exception $exception) {
+            http_response_code(400);
+            return json_encode(["error" => $exception->getMessage()]);
+        }
+    }
+}
